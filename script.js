2493b4f2a18e6504089481aaa9de31d9772eafec
Tom Jardine-McNamara <tom@itsravenous.com>
2020-07-06 18:26:11 +0100

    Remove WIP ui demo

--- script.js
+++ script.js
@@ -2,47 +2,6 @@ const $ = document.getElementById.bind(document);
 const $$ = document.querySelector.bind(document);
 const $$$ = document.querySelectorAll.bind(document);
 
-const phoneEl = $("phone");
-const messagesEl = $("messages");
-const inputEl = $("input");
-const menuEl = $("menu");
-const inventoryEl = $("inventory");
-const inventoryCloseEl = $("inventory-close");
-
-const COMMANDS = ["invite", "inventory", "objectives"];
-const matchCommands = commandPart =>
-  COMMANDS.filter(command => command.startsWith(commandPart));
-
-inputEl.addEventListener("input", () => {
-  const val = inputEl.value;
-  if (val[0] === "/") {
-    menuEl.classList.remove("hidden");
-    menuEl.innerHTML = matchCommands(val.slice(1))
-      .map(command => `<li id="${command}">${command}</li>`)
-      .join("");
-  } else {
-    menuEl.classList.add("hidden");
-  }
-});
-
-const HANDLERS = {
-  inventory: showInventory
-};
-menuEl.addEventListener("click", e => {
-  const item = e.target;
-  HANDLERS[item.id] && HANDLERS[item.id]();
-});
-
-function showInventory() {
-  inventoryEl.classList.remove("hidden");
-}
-
-function hideInventory() {
-  inventoryEl.classList.add("hidden");
-}
-
-inventoryCloseEl.addEventListener("click", hideInventory);
-
 // Join modals
 const modalLinks = $$$("[data-modal-id]");
 modalLinks.forEach(modalLink => {
